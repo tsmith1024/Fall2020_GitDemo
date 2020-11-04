c07378a8b85095a15d0fa0a510e6f417efc14c72
Taylor Smith <tsmith1024@gmail.com>
2020-11-03 19:13:26 -0600

    Initial Commit

---
+++ Fall2020_GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fall2020_GitDemo
+//
+//  Created by Taylor Smith on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
